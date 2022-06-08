2c3bb66b9686293baed6bb3eabab5fad2091c45d
JI SOO <dlwltn815@gmail.com>
2022-06-08 20:01:40 +0900

    Feat: xcode project생성

---
+++ ChagokChagok/ChagokChagok/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChagokChagok
+//
+//  Created by LeeJiSoo on 2022/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
