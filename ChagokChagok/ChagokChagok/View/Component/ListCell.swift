a2265e4fb8a57cda3283255623d6f9c936c5accc
JI SOO <dlwltn815@gmail.com>
2022-06-10 10:15:15 +0900

    Wip: list cell 생성해서 데이터 적용
    - 임시 list cell 생성 후 데이터 적용 확인 작업

--- ChagokChagok/ChagokChagok/View/Component/ListCell.swift
+++ ChagokChagok/ChagokChagok/View/Component/ListCell.swift
@@ -1,22 +1,33 @@
 import SwiftUI
 
 struct ListCell: View {
-    @Environment(\.managedObjectContext) var managedObjContext
-    @Environment(\.dismiss) var dismiss
-    @FetchRequest(sortDescriptors: [SortDescriptor(\.createTime, order: .reverse)]) var pin: FetchedResults<Pin>
-    @State private var name = ""
-    
-    @State private var latitude = 0.0
-    @State private var longitude = 0.0
+
+    @State var pinImageName: String
+    @State var pinName: String
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image(pinImageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text(pinName)
+                .foregroundColor(.black)
+                .fontWeight(.semibold)
+                .font(.footnote)
+                .lineLimit(2)
+            Spacer()
+        }
+        .frame(height: 60)
+        .background(Color.white.opacity(0.085))
+        .cornerRadius(5)
+
     }
 }
 
 struct ListCell_Previews: PreviewProvider {
     static var previews: some View {
-        ListCell()
+        ListCell(pinImageName: "temp", pinName: "Hello World")
+            .previewLayout(.sizeThatFits)
     }
 }
