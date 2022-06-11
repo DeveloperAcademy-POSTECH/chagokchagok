51d141540af20e27e3f579b7517faf109107bff5
JI SOO <dlwltn815@gmail.com>
2022-06-12 00:46:49 +0900

    Feat: SelectCategoryView 생성
    - 카테고리를 선택하는 view 파일 생성

---
+++ ChagokChagok/ChagokChagok/SelectCategoryView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SelectCategoryView: View {
+    var body: some View {
+        
+    }
+}
+
+struct SelectCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectCategoryView()
+    }
+}
