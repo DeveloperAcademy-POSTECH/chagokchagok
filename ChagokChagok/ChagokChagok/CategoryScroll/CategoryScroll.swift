860d3797038b0db91943c28ad171f9094ab08e62
yungahui <gahuiii99@gmail.com>
2022-06-12 21:54:40 +0900

    Feat: CategoryScroll 생성

---
+++ ChagokChagok/ChagokChagok/CategoryScroll/CategoryScroll.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryScroll.swift
+//  ChagokChagok
+//
+//  Created by 윤가희 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct CategoryScroll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryScroll_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryScroll()
+    }
+}
