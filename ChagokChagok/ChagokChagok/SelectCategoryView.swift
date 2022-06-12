569136202857f8a8ea483d11941b9ef42fe2c05a
JI SOO <dlwltn815@gmail.com>
2022-06-12 13:30:17 +0900

    Feat: Select Category View UI 생성
    - 이미지와 text로 이루어진 버튼 VIew UI 생성

--- ChagokChagok/ChagokChagok/SelectCategoryView.swift
+++ ChagokChagok/ChagokChagok/SelectCategoryView.swift
@@ -1,8 +1,42 @@
 import SwiftUI
 
 struct SelectCategoryView: View {
+//    해당 pin/course 가 어떤 카테고린지 받아와야함
+    @State var category = ""
+    let column = [GridItem(.adaptive(minimum: 100))]
+    
     var body: some View {
-        
+        VStack {
+            selectCategoryImage()
+        }
+    }
+    
+    private func selectCategoryImage() -> some View {
+        LazyVGrid(columns: column, spacing: 10) {
+            ForEach(CourseCategory.allCases, id: \.rawValue) { value in
+                Button(action: {
+                    category = value.rawValue
+                    // action
+                }, label: {
+                    VStack {
+                        Group {
+                            value.categoryImage
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                            selectCategoryText(value: value.rawValue)
+                        }
+                    }
+                })
+                .padding(.bottom, 42)
+            }
+        }
+        .padding(24)
+    }
+    
+    private func selectCategoryText(value: String) -> some View {
+        Text(value)
+            .font(.system(size: 18))
+            .foregroundColor(.black)
     }
 }
 
