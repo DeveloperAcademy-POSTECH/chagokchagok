6e9b1610e7045a9c4c90a2a9b194f204a8c44348
JI SOO <dlwltn815@gmail.com>
2022-06-13 21:44:22 +0900

    Refactor: list cell 코드 구조 변경 (#20)

--- ChagokChagok/ChagokChagok/ListCell.swift
+++ ChagokChagok/ChagokChagok/ListCell.swift
@@ -10,38 +10,51 @@ struct ListCell: View {
     
     var body: some View {
         HStack {
-            Image(image)
-                .listIconStyle()
-            VStack(alignment: .leading, spacing: 0) {
-                HStack {
-                    Image("tempTypeImage")
-                        .resizable()
-                        .frame(width: 13, height: 10, alignment: .leading)
-                    Text(type)
-                        .foregroundColor(.gray)
-                        .font(.system(size: 12))
-                }
-                .frame(width: 40, height: 20)
-                Text(name != nil ? name! : createTime)
-                    .listTitleStyle()
-                    .listTextSpaceStyle()
-                Text(memo != nil ? "\(memo!)" : "메모를 입력해주세요.")
-                    .listMemoStyle()
-                    .listMemoSpaceStyle()
-            }
+            listImage
+            listText
             Spacer()
-            VStack {
-                Image(systemName: isFavorite ? "heart.fill" : "heart")
-                    .frame(width: 20, height: 20, alignment: .topTrailing)
-                    .padding(.top, 5)
-//                    .onTapGesture {
-//                        isFavorite.toggle()
-//                    }
-                Spacer()
-            }
+            isfavoriteBtn
         }
         .frame(width: 350, height: 92)
     }
+    
+    var listImage: some View {
+        Image(image)
+            .listIconStyle()
+    }
+    
+    var listText: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Image("tempTypeImage")
+                    .resizable()
+                    .frame(width: 13, height: 10, alignment: .leading)
+                Text(type)
+                    .foregroundColor(.gray)
+                    .font(.system(size: 12))
+            }
+            .frame(width: 40, height: 20)
+            Text(name != nil ? name! : createTime)
+                .listTitleStyle()
+                .listTextSpaceStyle()
+            Text(memo != nil ? "\(memo!)" : "메모를 입력해주세요.")
+                .listMemoStyle()
+                .listMemoSpaceStyle()
+        }
+    }
+    
+    var isfavoriteBtn: some View {
+        VStack {
+            Image(systemName: isFavorite ? "heart.fill" : "heart")
+                .frame(width: 20, height: 20, alignment: .topTrailing)
+                .padding(.top, 5)
+//                    .onTapGesture {
+//                        isFavorite.toggle()
+//                    }
+            Spacer()
+        }
+    }
+    
 }
 
 struct ListCell_Previews: PreviewProvider {
