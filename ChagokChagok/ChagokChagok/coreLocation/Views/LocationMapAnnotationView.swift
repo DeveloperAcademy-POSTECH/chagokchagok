793a044715f005749d1514b1edaf1383a1c6da9d
SeonJeon <letschutzpah430@gmail.com>
2022-06-14 14:34:36 +0900

    Feat: 핀 이미지 구현
    맵 화면에 스팟을 가르키는 핀 이미지를 구현했습니다.

---
+++ ChagokChagok/ChagokChagok/coreLocation/Views/LocationMapAnnotationView.swift
@@ -0,0 +1,43 @@
+//
+//  LocationMapAnnotationView.swift
+//  ChagokChagok
+//
+//  Created by seojeon22 on 2022/06/14.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    
+    let accentColor = Color("AccentColor")
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "location.fill.viewfinder")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 25, height: 25)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(6)
+                .background(accentColor)
+                .cornerRadius(10)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(accentColor)
+                .frame(width: 8, height: 8)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -1)
+                .padding(.bottom, 20)
+        }
+    }
+}
+
+struct LocationMapAnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationMapAnnotationView()
+    }
+}
+
