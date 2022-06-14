8bf65b46892a34efac3c0e51c2d65b73132b3558
yungahui <gahuiii99@gmail.com>
2022-06-14 09:59:59 +0900

    Feat: 코스리스트뷰 UI껍데기 구현 (#23)

--- ChagokChagok/ChagokChagok/CourseListView.swift
+++ ChagokChagok/ChagokChagok/CourseListView.swift
@@ -10,8 +10,18 @@ import SwiftUI
 struct CourseListView: View {
     var body: some View {
         VStack(spacing: 0) {
+            HStack(spacing: 0){
+                Image(systemName: "chevron.left")
+                    .font(.system(size: 24, weight: .medium))
+                    .padding(.trailing, 15.0)
+                Text("내 코스")
+                    .font(.system(size: 24, weight: .bold))
+            }
+            .padding(.leading, 20.0)
+            .padding(.top, 60.0)
+            .frame(maxWidth: .infinity, alignment: .leading)
             CategoryScroll()
-                .padding(.top, 123.0)
+                .padding(.top, 33.0)
                 .padding(.leading, 20.0)
             Text("Total ")
                 .font(.system(size: 14, weight: .regular))
